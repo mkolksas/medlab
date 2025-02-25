a4837abfb27ec54ba962d4617849d74b39ba0050
eddamax0403 <mwanziaedwin5@gmail.com>
2025-02-25 16:33:57 +0300

    pushi

--- HospitalApp/urls.py
+++ HospitalApp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('starter/',views.starter,name='starter'),
+    path('about/',views.about,name='about'),
+    path('services/',views.services,name='services'),
+    path('departments/',views.departments,name='departments'),
+    path('doctors/',views.doctors,name='doctors'),
 ]
